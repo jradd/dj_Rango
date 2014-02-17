8ca05b136cfeaf2e52092dc196146b702a53ee25
jradd <jredd42@gmail.com>
2014-02-17 02:26:56 -0700

    Post admin tests in place

--- blogengine/tests.py
+++ blogengine/tests.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 from blogengine.models import Post
 
 class PostTest(TestCase):
-    def test_create_post(self):
-        # Create the Postpost
+   def test_create_post(self):
+        # Create the post
         post = Post()
 
         # Set the attributes
@@ -15,7 +15,7 @@ class PostTest(TestCase):
         # Save it
         post.save()
 
-        # Check to make sure we can find it
+        # Check we can find it
         all_posts = Post.objects.all()
         self.assertEquals(len(all_posts), 1)
         only_post = all_posts[0]
@@ -29,7 +29,8 @@ class PostTest(TestCase):
         self.assertEquals(only_post.pub_date.year, post.pub_date.year)
         self.assertEquals(only_post.pub_date.hour, post.pub_date.hour)
         self.assertEquals(only_post.pub_date.minute, post.pub_date.minute)
-        self.assertEquals(only_post.pub_date.second, post.pub_date.second)
+        self.assertEquals(only_post.pub_date.second, post.pub_date.second) 
+ 
 
 class AdminTest(LiveServerTestCase):
     fixtures = ['users.json'] 
@@ -84,3 +85,89 @@ class AdminTest(LiveServerTestCase):
 
         # Check 'Log in' in response
         self.assertTrue('Log in' in response.content)
+
+    def test_create_post(self):
+        # Log in
+        self.client.login(username='thedoctor', password="#EDC4rfv")
+
+        # Check response code
+        response = self.client.get('/admin/blogengine/post/add/')
+        self.assertEquals(response.status_code, 200)
+
+        # Create the Postpost
+        response = self.client.post('/admin/blogengine/post/add/', {
+                'title': 'My first post',
+                'text': 'This is my first post',
+                'pub_date_0': '2013-12-28',
+                'pub_date_1': '22:00:04'
+        },
+        follow=True
+        )
+        self.assertEquals(response.status_code, 200)
+
+        # Check added correctly
+        self.assertTrue('added successfully' in response.content)
+
+        # Check new post now in database
+        all_posts = Post.objects.all()
+        self.assertEquals(len(all_posts), 1)
+
+    def test_edit_post(self):
+        # Create the post
+        post = Post()
+        post.title = 'My first post'
+        post.text = 'This is my first blog post'
+        post.pub_date = timezone.now()
+        post.save()
+
+        # Log in
+        self.client.login(username='thedoctor', password="#EDC4rfv")
+
+        # Edit the post
+        response = self.client.post('/admin/blogengine/post/1/', {
+            'title': 'My second post',
+            'text': 'This is my second blog post',
+            'pub_date_0': '2013-12-28',
+            'pub_date_1': '22:00:04'
+        },
+        follow=True
+        )
+        self.assertEquals(response.status_code, 200)
+
+        # Check changed successfully
+        self.assertTrue('changed successfully' in response.content)
+
+        # Check post amended
+        all_posts = Post.objects.all()
+        self.assertEquals(len(all_posts), 1)
+        only_post = all_posts[0]
+        self.assertEquals(only_post.title, 'My second post')
+        self.assertEquals(only_post.text, 'This is my second blog post')
+
+    def test_delete_post(self):
+        # Create the post
+        post = Post()
+        post.title = 'My first post'
+        post.text = 'This is my first blog post'
+        post.pub_date = timezone.now()
+        post.save()
+
+        # Check new post saved
+        all_posts = Post.objects.all()
+        self.assertEquals(len(all_posts), 1)
+
+        # Log in
+        self.client.login(username='thedoctor', password="#EDC4rfv")
+
+        # Delete the post
+        response = self.client.post('/admin/blogengine/post/1/delete/', {
+            'post': 'yes'
+        }, follow=True)
+        self.assertEquals(response.status_code, 200)
+
+        # Check deleted successfully
+        self.assertTrue('deleted successfully' in response.content)
+
+        # Check post amended
+        all_posts = Post.objects.all()
+        self.assertEquals(len(all_posts), 0)
